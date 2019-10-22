e3855fd1794e6b50aa31abc2774e160f91780211
Sean DiGirolamo <tug70176@temple.edu>
2019-10-22 11:46:19 -0400

    Added necessary classes from other projects

--- src/edu/temple/cis/capstone/BinBotDemo1/Main.java
+++ src/edu/temple/cis/capstone/BinBotDemo1/Main.java
@@ -1,37 +1,8 @@
 package edu.temple.cis.capstone.BinBotDemo1;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.util.Arrays;
-
 public class Main
 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
-	
-	private static BufferedImage stringToBufferedImage(String s) {
-		BufferedImage retval = null;
-		try {
-			retval = ImageIO.read(new ByteArrayInputStream(s.getBytes()));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return retval;
-	}
-
-	private static String bufferedImageToString(BufferedImage bi) {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		try {
-			ImageIO.write(bi, "jpg", out);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return Arrays.toString(out.toByteArray());
-
-	}
-
 }
