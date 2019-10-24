24ba995d90b080af9dd257568973e7f5242f7dcb
Sean DiGirolamo <tug70176@temple.edu>
2019-10-24 17:21:41 -0400

    Successfully sends imgs embedded in json over a socket

--- src/edu/temple/cis/capstone/BinBotDemo1/Main.java
+++ src/edu/temple/cis/capstone/BinBotDemo1/Main.java
@@ -6,6 +6,9 @@ import edu.temple.cis.capstone.BinBotDemo1.instructions.Status;
 import edu.temple.cis.capstone.BinBotDemo1.instructions.TreadInstruction;
 import javafx.util.Pair;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -17,18 +20,24 @@ public class Main
 	static ServerConnection connection;
 
 	public static void main(String[] args) throws IOException {
+		String path = "C:/Users/Sean/Desktop/Client/";
+		String recieveFilename = "recieved.jpg";
 		System.out.println("Connecting to " + IP + " " + PORT + "...");
 		connection = new ServerConnection(IP, PORT);
 		System.out.println("Connection established!");
 
-		List<Pair<Double, Double>> l = TreadInstruction.calcInstructions(0, 0, 0, 0);
-
-		Instruction instruction = new Instruction(Status.NAVIGATE, null, l, null);
 		System.out.println("Attempting to recieve");
-		System.out.println("received: " + connection.recieve());
-		System.out.print("Sending: ");
-		System.out.println(instruction.json());
-		connection.send(instruction.json());
+		String jsonRecieve = connection.recieve();
+		System.out.println("received: " + jsonRecieve);
+		Instruction instrRecieve = new Instruction(jsonRecieve);
+		System.out.println("Saving image at " + path + recieveFilename);
+		ImageIO.write(instrRecieve.img(), "jpg", new File(path + recieveFilename));
+		System.out.println("saved!");
+
+		BufferedImage img = ImageIO.read(new File(path + recieveFilename));
+		Instruction instrSend = new Instruction(Status.NAVIGATE, img, null, null);
+		System.out.print("Sending: " + instrSend.json());
+		connection.send(instrSend.json());
 		System.out.println("Sent");
 	}
 }
