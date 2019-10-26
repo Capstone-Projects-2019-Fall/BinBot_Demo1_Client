714f85f4e798004d8af089c4261932f9604729e4
Sean <SeanR1194@gmail.com>
2019-10-25 21:32:36 -0400

    Added documentation

--- src/BinBotDemo1/MyFrame.java
+++ src/BinBotDemo1/MyFrame.java
@@ -5,15 +5,24 @@ import BinBotDemo1.instructions.Instruction;
 import BinBotDemo1.instructions.Status;
 import org.opencv.core.Core;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
+/**
+ * JFrame class to display video feed including the bounding boxes of the object detector results. Using the
+ * VideoCap class, the instance's thread captures a BufferedImage which is sent to the processing server. The server
+ * will return the BufferedImage with the resulting bounding boxes.
+ *
+ * @author Sean Reddington
+ * @version 1.0
+ * @since 2019-10-25
+ */
 public class MyFrame extends JFrame {
 
+    // Loading the OpenCV core library
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
@@ -26,6 +35,14 @@ public class MyFrame extends JFrame {
 
     static ServerConnection connection;
 
+    /**
+     * Main class to run the client portion of team BinBot's first demo. Establishes a connection to the BinBot server,
+     * then begins the live feed object detection within a JFrame.
+     *
+     * @author Sean Reddington
+     * @version 1.0
+     * @since 2019-10-25
+     */
     public static void main(String[] args) throws IOException {
         setup();
         EventQueue.invokeLater(() -> {
@@ -41,8 +58,14 @@ public class MyFrame extends JFrame {
 
     }
 
+    /**
+     * Configures JPanel for video feed display and starts thread.
+     *
+     * @author Sean Reddington
+     * @version 1.0
+     * @since 2019-10-25
+     */
     public MyFrame() {
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(0, 0, 1280, 720);
         contentPane = new JPanel();
@@ -54,14 +77,27 @@ public class MyFrame extends JFrame {
 
     }
 
+    /**
+     * Configures connection to the BinBot processing server via the ServerConnection class.
+     *
+     * @author Sean Reddington
+     * @version 1.0
+     * @since 2019-10-25
+     */
     public static void setup() throws IOException {
         System.out.println("Connecting to " + IP + " " + PORT + "...");
         connection = new ServerConnection(IP, PORT);
         System.out.println("Connection established!");
     }
 
+    /**
+     * Sends the passed BufferedImage to the BinBot processing server via the ServerConnection instance.
+     *
+     * @author Sean Reddington
+     * @version 1.0
+     * @since 2019-10-25
+     */
     public static void sendCapture(BufferedImage image) {
-        // Send to server
         Instruction imgSend = new Instruction(Status.NAVIGATE, image, null, null);
 //        System.out.print("Sending: " + imgSend.json());
         System.out.print("Sending image to server..");
@@ -73,6 +109,13 @@ public class MyFrame extends JFrame {
         System.out.println("Sent");
     }
 
+    /**
+     * Receives a BufferedImage from the BinBot processing server via the ServerConnection instance.
+     *
+     * @author Sean Reddington
+     * @version 1.0
+     * @since 2019-10-25
+     */
     public static BufferedImage receiveCapture() {
         System.out.println("Attempting to receive");
         String jsonReceive = null;
@@ -87,15 +130,30 @@ public class MyFrame extends JFrame {
         return imgReceive.img();
     }
 
+    /**
+     * Captures a BufferedImage from the video feed and sends it to the BinBot processing server. The server will send
+     * a modified BufferedImage containing bounding boxes of any object detected from the machine learning model.
+     *
+     * @author Sean Reddington
+     * @version 1.0
+     * @since 2019-10-25
+     */
+    @Override
     public void paint(Graphics g) {
         g = contentPane.getGraphics();
-        BufferedImage image = videoCap.getOneFrame();
-        sendCapture(image);
-        image = receiveCapture();
-
-        g.drawImage(image, 0, 0, this);
+        BufferedImage image = videoCap.getOneFrame(); // Capture a frame to send to server
+        sendCapture(image); // Send the BufferedImage to the server
+        image = receiveCapture(); // Receive the modified BufferedImage back from the server
+        g.drawImage(image, 0, 0, this); // Draw the new image to the JFrame
     }
 
+    /**
+     * Thread class to handle the JFrame
+     *
+     * @author Sean Reddington
+     * @version 1.0
+     * @since 2019-10-25
+     */
     class MyThread extends Thread {
         @Override
         public void run() {
--- src/BinBotDemo1/VideoCap.java
+++ src/BinBotDemo1/VideoCap.java
@@ -8,8 +8,17 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
 
+
+/**
+ * Class to record video feed from a connected device and capture individual frames as BufferedImages
+ *
+ * @author Sean Reddington
+ * @version 1.0
+ * @since 2019-10-25
+ */
 public class VideoCap {
 
+    // Loading the OpenCV core library
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
@@ -17,24 +26,43 @@ public class VideoCap {
     VideoCapture capture;
     Mat mat = new Mat();
 
-
-    VideoCap() {
+    /**
+     * Instantiates the VideoCapture class and opens the camera instance
+     *
+     * @author Sean Reddington
+     * @version 1.0
+     * @since 2019-10-25
+     */
+    public VideoCap() {
         capture = new VideoCapture();
         capture.open(0);
     }
 
-    BufferedImage getOneFrame() {
-//        capture.read(mat2Image.mat);
-//        return mat2Image.getImage(mat2Image.mat);
-
-        if (capture.read(this.mat)) {
-            BufferedImage img = new BufferedImage(mat.width(), mat.height(), BufferedImage.TYPE_3BYTE_BGR);
-            WritableRaster raster = img.getRaster();
-            DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
-            byte[] data = dataBuffer.getData();
-            mat.get(0, 0, data);
-            return img;
+    /**
+     * Attempts to capture a frame from the VideoCapture feed and converts it to a BufferedImage.
+     *
+     * @return BufferedImage or null on failure
+     * @author Sean Reddington
+     * @version 1.0
+     * @since 2019-10-25
+     */
+    public BufferedImage getOneFrame() {
+        if (capture.isOpened()) {
+            if (capture.read(this.mat)) { // Capture frame into mat
+                //create a BufferedImage from the frame capture
+                BufferedImage img = new BufferedImage(mat.width(), mat.height(), BufferedImage.TYPE_3BYTE_BGR);
+                WritableRaster raster = img.getRaster();
+                DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+                byte[] data = dataBuffer.getData();
+                mat.get(0, 0, data);
+                return img;
+            } else {
+                System.err.println("Failed to capture frame!");
+                return null;
+            }
+        } else {
+            System.err.println("Failed to capture frame -- video capture closed!");
+            return null;
         }
-        return null;
     }
 }
